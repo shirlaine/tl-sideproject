8a07a156ceceeee13fcaf52d4d1814db8b7bf7a8
shirlaine <shirlaine.phang@gmail.com>
2017-08-09 16:08:25 +0800

    Add post controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ root to: 'users#new'
  get '/users/edit', to: 'users#edit'
 
 get '/users/support-us', to: 'users#support_us'
+
+get '/posts', to:'posts#index'
+
+get '/posts/show', to: 'posts#show'
+
 end

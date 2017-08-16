34ed9e38976ed2b04ed34f921f83dac618a692a7
shirlaine <shirlaine.phang@gmail.com>
2017-08-16 13:47:39 +0800

    Update bootcamp model validations

--- app/models/bootcamp.rb
+++ app/models/bootcamp.rb
@@ -1,3 +1,14 @@
-class Bootcamp < ActiveRecord::Base
+class Bootcamp < ApplicationRecord
+  validates :name, :startdate, :enddate, presence: true
+
+  validate :enddate_cannot_be_before_startdate
+
+  private
+
+  def enddate_cannot_be_before_startdate
+    if enddate<=startdate
+      errors.add(:enddate, "cannot be before the start date")
+    end
+  end
 
 end
